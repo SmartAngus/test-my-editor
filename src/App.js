6cd09398fbd7ae534243aea140b600c8d73b4df5
AnnDon <majy89@126.com>
2020-10-20 14:14:24 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { DataVEditor } from 'smart-datav-npm'
+import 'antd/dist/antd.css'
+import 'smart-datav-npm/dist/index.css'
 
 function App() {
+  const handleSaveEditorData = (data:any)=>{
+    console.log(data)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header style={{height:50,width:'100%',background:'red'}}>header</header>
+      <div style={{width:'10%',height:'100vh',float:'left'}}>left</div>
+      <div style={{width:'89%',height:'100vh',float:'right'}}>
+        <DataVEditor  onEditorSaveCb={handleSaveEditorData}/>
+      </div>
+
     </div>
   );
 }
